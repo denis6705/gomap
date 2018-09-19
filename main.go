19df3f85ffae0731b8549b110d8275fb8d047ac2
Denis Shkabatur <denis6705@gmail.com>
2018-09-19 20:28:47 +0300

    add -host flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"time"
@@ -9,18 +10,24 @@ import (
 )
 
 var (
-	timeout   = time.Second
-	ping_time time.Duration
-	attempts  uint = 3
-
+	timeout    = time.Second
+	ping_time  time.Duration
+	attempts   uint = 3
+	host       string
 	remoteAddr *net.IPAddr
 	pinger     *ping.Pinger
 )
 
 func main() {
+	flag.StringVar(&host, "host", host, "usage")
+	flag.Parse()
 	pinger, _ = ping.New("0.0.0.0", "")
-	remoteAddr, _ = net.ResolveIPAddr("ip4", "google.com")
-	ping_time, _ = pinger.PingAttempts(remoteAddr, timeout, 3)
-	fmt.Println("time: ", ping_time)
+	remoteAddr, _ = net.ResolveIPAddr("ip4", host)
+	if remoteAddr != nil {
+		ping_time, _ = pinger.PingAttempts(remoteAddr, timeout, 3)
+		fmt.Println("host: ", host, " time: ", ping_time)
+	} else {
+		fmt.Println("ipaddress is bad!")
+	}
 
 }
